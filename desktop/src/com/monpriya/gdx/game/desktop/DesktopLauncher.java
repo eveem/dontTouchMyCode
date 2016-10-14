213863a640273782fe745d383bee3740b99a56ff
eveem <monpriya.tammavong@gmail.com>
2016-10-14 14:36:13 +0700

    Set width and height

--- desktop/src/com/monpriya/gdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/monpriya/gdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.monpriya.gdx.game.ProjectGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1200;
+		config.height = 800;
 		new LwjglApplication(new ProjectGame(), config);
 	}
 }
